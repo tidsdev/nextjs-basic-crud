12aeb63a7dd3a8972eb9d1e116a06810a05375b0
ติ๊ดส์ <tidswork7@gmail.com>
2025-04-08 16:02:18 +0700

    Update Edit Log

--- src/app/edit/[id]/page.jsx
+++ src/app/edit/[id]/page.jsx
@@ -18,7 +18,7 @@ function EditPostPage({ params }) {
 
   const getPostById = async (id) => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts/${id}`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/posts/${id}`, {
         method: "GET",
         cache: "no-store",
       });
@@ -32,6 +32,7 @@ function EditPostPage({ params }) {
       setPostData(data.post);
     } catch (error) {
       console.log(error);
+      alert("Error Edit Get Post: ", error , "NEXT_PUBLIC_API_BASE_URL :" , process.env.NEXT_PUBLIC_API_URL);
     }
   };
 
