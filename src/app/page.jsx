3aeb3f118d3bb0ff8dc330c33a6591d4f17b7ce1
ติ๊ดส์ <tidswork7@gmail.com>
2025-04-08 14:31:56 +0700

    Fix API base URL for production

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const getPosts = async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/posts", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, {
         method: "GET",
         headers: {
           "content-type": "application/json",
